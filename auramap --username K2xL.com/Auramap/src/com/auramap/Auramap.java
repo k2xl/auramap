e7df9add9a0be6669e0e22aafe4b4a4086b5f89b
k2xl.com <k2xl.com@4b583d28-6101-11de-92c7-cf56be1892c0>
2009-06-24 20:59:15 +0000

    Initial import.

---
+++ auramap --username K2xL.com/Auramap/src/com/auramap/Auramap.java
@@ -0,0 +1,13 @@
+package com.auramap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Auramap extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
