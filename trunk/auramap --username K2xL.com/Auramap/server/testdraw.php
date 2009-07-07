f52eeca8f6a4bf823f8317ceefbb884840f12b8c
k2xl.com <k2xl.com@4b583d28-6101-11de-92c7-cf56be1892c0>
2009-07-07 18:46:28 +0000

    Server stuff, circles drawn on map

--- trunk/auramap --username K2xL.com/Auramap/server/testdraw.php
+++ trunk/auramap --username K2xL.com/Auramap/server/testdraw.php
@@ -5,25 +5,47 @@
 
  
 // set the HTTP header type to PNG
-header("Content-type: image/png"); 
- 
+header("Content-type: image/png");
+require_once("login.php");
 // set the width and height of the new image in pixels
 
-$circlealpha = 0.05;
+$circlealpha = .5;//0.05;
 $radius = 50;
 $imgSrc = "gtmap.JPG";
 
+$topleftlat = $Header['topleftlat'];//33.78156339080061;
+$topleftlong = $Header['topleftlong'];//-84.40765857696533;
+$bottomrightlat =  $Header['bottomrightlat'];//33.771075867373405;
+$bottomrightlong = $Header['bottomrightlong'];//-84.38984870910645; 
 $myImage = imagecreatefromjpeg($imgSrc); 
-// send the new PNG image to the browser
-//ImagePNG($im); 
+// send the new PNG image to the browser 
 
 
 $width = imagesx($myImage);
 $height = imagesy($myImage);
+
+
+$sql = "Select * from aurapoints where lat<$topleftlat and lat>$bottomrightlat and lon>$topleftlong and lon<$bottomrightlong";
+
+$DataPoints = $DB->query("$sql");
+
+/*
+ * $black = imagecolorallocate($myImage, 0x00, 0x00, 0x00);
+*	imagefttext($myImage, 13, 0, 0, 55, $black, "fonts/ARIAL.TTF", "Results:$x,$y");
+ */
 // Parse through all the circles
-for ($a = 0; $a < 1000; $a++){
-	$x = rand($radius,$width-$radius);
-	$y = rand($radius,$height-$radius);
+
+while ($rs = mysql_fetch_assoc($DataPoints))
+{	
+	$percX = (floatval($rs['lon']-$topleftlong)/($bottomrightlong-$topleftlong));
+	$percY =  (floatval($rs['lat']-$topleftlat)/($bottomrightlat-$topleftlat));
+	
+	$x = $percX*$width;
+	$y = $percY*$height;
+	
+	//$x = rand($radius,$width-$radius);
+	//$y = rand($radius,$height-$radius);
+	
 	$r = rand(0,256);
 	$b = rand(0,256);
 	$g = rand(0,256);
@@ -34,6 +56,6 @@ for ($a = 0; $a < 1000; $a++){
 imagepng($myImage);
 
 // destroy the reference pointer to the image in memory to free up resources
-ImageDestroy($im); 
+ImageDestroy($myImage); 
 
 ?>
