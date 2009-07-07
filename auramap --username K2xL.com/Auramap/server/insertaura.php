3f18bbf02568884dd4d61b8e3f666262acea2838
k2xl.com <k2xl.com@4b583d28-6101-11de-92c7-cf56be1892c0>
2009-07-07 18:46:28 +0000

    Server stuff, circles drawn on map

--- auramap --username K2xL.com/Auramap/server/insertaura.php
+++ auramap --username K2xL.com/Auramap/server/insertaura.php
@@ -3,7 +3,7 @@ require_once("login.php");
 
 // Step 1: Secure parameters
 $lat = floatval($Headers['lat']);
-$long = floatval($Headers['long']);
+$long = floatval($Headers['lon']);
 $emotx = intval($Headers['emotx']);
 $emoty = intval($Headers['emoty']); 
 $tag = $Headers['tag'];
@@ -11,15 +11,18 @@ $tag = $Headers['tag'];
 $Data = array();
 $Data['user_id']= $Me['id'];
 $Data['tag'] = "'$tag'";
-$Data['lat'] = $lat;
-$Data['long'] = $long;
-$Data['emotx'] = $emotx;
-$Data['emoty'] = $emoty;
+$Data['lat'] = "'$lat'";
+$Data['lon'] = "'$long'";
+$Data['emotx'] = "'$emotx'";
+$Data['emoty'] = "'$emoty'";
 
 $insertOK = $DB->insert("aurapoints",$Data);
 if (!$insertOK)
 {
-	return SERVER_ERROR;
+	echo SERVER_ERROR;
+	exit();
 }
+
+echo SUCCESS;
 
 ?>
--- auramap --username K2xL.com/Auramap/server/login.php
+++ auramap --username K2xL.com/Auramap/server/login.php
@@ -7,12 +7,16 @@
  require_once("utils/sp_session.class.php");
  require_once("utils/rss_php.class.php");
 // CLEAN UP POST AND GET... USE A PHP INPUT FILTER!
-header("Content-type: text/xml");
-$Headers = $_GET;
+//header("Content-type: text/xml");
+$Headers = $_POST;
+if (isset($Headers['username'])== false)
+{
+	$Headers = $_GET;
+}
 $username = $Headers['username'];
 $password = md5($Headers['password']);
- 
-$DB = new mysql_helper("localhost","root","","auramap");
+
+$DB = new mysql_helper("localhost","mtserver2","X:JyPz.p7TyNSSPD","auramap");
 $DB->setDebug(true);
  
 $loggedIn = $DB->count("users","where username='$username' and password='$password'");
