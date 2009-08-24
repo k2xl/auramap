7511c1eff1e52d50971a9f62a4ac0e601eb2d0f3
k2xl.com@gmail.com <k2xl.com@gmail.com@4b583d28-6101-11de-92c7-cf56be1892c0>
2009-08-24 05:13:01 +0000

    Fixed other bugs

--- auramap --username K2xL.com/Auramap/server/getBuddyList.php
+++ auramap --username K2xL.com/Auramap/server/getBuddyList.php
@@ -9,9 +9,13 @@ while ($rs = mysql_fetch_assoc($buddies))
 	$num = ($rs['buddynumber']);
 	$privacy = ($rs['privacy']);
 	$userID = getUserWithPhone($num);
+	if ($userID == false) { $userID = -1; }
+	else { $userID = $userID['id']; }
+	
 	$permissions = -1;
 	$lastupdate = 0;
 	$happystate = -1;
+	
 	if ($userID > 0){
 		$lastAura = getLatestAurapoint($userID);
 		$happystate = $lastAura['emotrating'];
@@ -26,10 +30,11 @@ function getLatestAurapoint($userID)
 {
 	global $DB;
 	// Phone has been registered... Now I would check here if the $userID has given the $id permissions... but i'll save that for another day
-	$latestAurapoint = $DB->query("select * from aurapoints where userid=$userID limit 1");
+	$sql = "select * from aurapoints where user_id=$userID order by timestamp desc limit 0,1";
+	$latestAurapoint = $DB->query($sql);
 	if (!$latestAurapoint) { echo SERVER_ERROR; exit(); }
 	$latestAurapoint = mysql_fetch_assoc($latestAurapoint);
-	
+
 	return $latestAurapoint;
 }
 ?>
