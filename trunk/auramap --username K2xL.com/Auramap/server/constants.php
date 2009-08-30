3df54e8fbdf26f950c362fa833ad6c33192fabe6
k2xl.com@gmail.com <k2xl.com@gmail.com@4b583d28-6101-11de-92c7-cf56be1892c0>
2009-08-30 23:20:42 +0000

    ping.php, a bunch of changes, lots of cool stuff

--- trunk/auramap --username K2xL.com/Auramap/server/constants.php
+++ trunk/auramap --username K2xL.com/Auramap/server/constants.php
@@ -10,13 +10,14 @@
  define("INVALID_CHANGE_REQUEST_ERROR","INVALID_CHANGE_REQUEST_ERROR");
  define("POST_MESSAGE_LENGTH_ERROR","POST_MESSAGE_LENGTH_ERROR");
  define("SERVER_ERROR","SERVER_ERROR");
- define("ALREADY_VOTED_ERROR","ALREADY_VOTED_ERROR");
+ define("ALREADY_NUDGED","ALREADY_NUDGED");
  define("DUPLICATE_ERROR","DUPLICATE_ERROR");
  define("EMPTY_RESULT","EMPTY_RESULT");
  define("PARAMETER_ERROR","PARAMETER_ERROR");
  define("INVALID_PHONE_ERROR","INVALID_PHONE_ERROR");
  define("MAX_NUDGES_REACHED","MAX_NUDGES_REACHED");
  define("UNKNOWN_PHONE","UNKNOWN_PHONE");
+ define("NO_NUDGES","0");
  // VALUES
  define("NUDGE_EXPIRATION",60*60*24); // in minutes
  define("MAX_ACTIVE_NUDGES",5); // in minutes
@@ -36,4 +37,64 @@
   	else
      return false;
 }
+
+
+function distanceOfTimeInWords($distanceInSeconds, $showLessThanAMinute = false) 
+{
+	return $distanceInSeconds; /*
+	
+    $distanceInMinutes = round($distanceInSeconds / 60);
+        
+        if ( $distanceInMinutes <= 1 ) {
+            if ( !$showLessThanAMinute ) {
+                return ($distanceInMinutes == 0) ? 'less than a minute ago' : '1 minute ago';
+            } else {
+                if ( $distanceInSeconds < 5 ) {
+                    return 'less than 5 seconds ago';
+                }
+                if ( $distanceInSeconds < 10 ) {
+                    return 'less than 10 seconds ago';
+                }
+                if ( $distanceInSeconds < 20 ) {
+                    return 'less than 20 seconds ago';
+                }
+                if ( $distanceInSeconds < 40 ) {
+                    return 'about half a minute ago';
+                }
+                if ( $distanceInSeconds < 60 ) {
+                    return 'less than a minute ago';
+                }
+                
+                return '1 minute ago';
+            }
+        }
+        if ( $distanceInMinutes < 45 ) {
+            return $distanceInMinutes . ' minutes ago';
+        }
+        if ( $distanceInMinutes < 90 ) {
+            return 'about 1 hour ago';
+        }
+        if ( $distanceInMinutes < 1440 ) {
+            return 'about ' . round(floatval($distanceInMinutes) / 60.0) . ' hours ago';
+        }
+        if ( $distanceInMinutes < 2880 ) {
+            return '1 day';
+        }
+        if ( $distanceInMinutes < 43200 ) {
+            return 'about ' . round(floatval($distanceInMinutes) / 1440) . ' days ago';
+        }
+        if ( $distanceInMinutes < 86400 ) {
+            return 'about 1 month';
+        }
+        if ( $distanceInMinutes < 525600 ) {
+            return round(floatval($distanceInMinutes) / 43200) . ' months ago';
+        }
+        if ( $distanceInMinutes < 1051199 ) {
+            return 'about 1 year ago';
+        }
+        
+        return 'over ' . round(floatval($distanceInMinutes) / 525600) . ' years ago';
+        */
+}
+ 
 ?>
---
+++ trunk/auramap --username K2xL.com/Auramap/server/ping.php
@@ -0,0 +1,80 @@
+<?php
+require_once("login.php");
+$myusername = $Me['username'];
+$myuserid = $Me['id'];
+
+if (isset($Headers['clearnudges']))
+{
+	if ($Headers['clearnudges'] == 1)
+	{
+		$Update = array();
+		$Update['expired'] = 1;
+		$Update['read_timestamp'] = "UNIX_TIMESTAMP()";
+		$q = $DB->update("nudges",$Update,"where target_number=$myusername");
+		if (!$q){ echo SERVER_ERROR; exit();}
+		
+		
+		echo SUCCESS;
+		exit();
+	}
+	else if ($Headers['clearnudges'] == 2)
+	{
+		$Update = array();
+		$Update['expired'] = 3;
+		$q = $DB->update("nudges",$Update, "where user_id=$myuserid and expired=2");
+		
+		echo SUCCESS." [q= $q]";
+		exit();
+	}
+	echo PARAMETER_ERROR; exit();
+}
+$activeNudges = $DB->query("select (select username from users where id=nudges.user_id) as phone from nudges where target_number=$myusername and expired=0");
+if (!$activeNudges)
+{
+	echo SERVER_ERROR; exit();
+}
+$count = mysql_num_rows($activeNudges);
+if ($count== 0)
+{
+	echo NO_NUDGES;
+	//	exit();
+}
+else{
+	echo "1,;,";
+	while ($rs = mysql_fetch_assoc($activeNudges))
+	{
+		echo $rs['phone'].",;,";
+	}
+	// get all your nudges that have been seen
+}
+echo "#";
+$yourReadNudges = $DB->query("select timestamp,(select user_id from users where username=nudges.target_number) as target_id,read_timestamp,target_number from nudges where user_id=$myuserid and read_timestamp>0 and expired<=2");
+if (!$yourReadNudges)
+{
+	echo SERVER_ERROR; exit();
+}
+if (mysql_num_rows($yourReadNudges) == 0)
+{
+	echo "0";
+	exit();
+}
+while ($rs = mysql_fetch_assoc($yourReadNudges))
+{
+	// now check to see if this person has updated their status since
+	$targetid = $rs['target_id'];
+	$q = $DB->query("select * from aurapoints where user_id=$targetid order by timestamp desc limit 0,1");
+	if (!$q) {echo SERVER_ERROR; exit();}
+	$row = mysql_fetch_assoc($q);
+	if (isset($row['timestamp']) == false) { continue; }
+	
+	if ($row['timestamp'] < $rs['timestamp']) { continue; }
+	
+	$Update = array();
+	$Update['expired'] = 2;
+	
+	$q = $DB->update("nudges",$Update, "where target_number=".$rs['target_number']." and user_id=$myuserid and expired=1");
+	if (!$q) { echo SERVER_ERROR; exit(); }	
+	echo $rs['target_number'].",;,".$row['timestamp'].",;,".$row['emotrating'].",;,".$row['tag'].";;;";
+}
+
+?>
